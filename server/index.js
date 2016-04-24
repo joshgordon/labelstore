60965d05e88f85e27d28221b391ebc5fd0844824
Josh Gordon <github@joshgordon.net>
2016-04-23 22:24:11 -0400

    Fixing port number for heroku

--- server/index.js
+++ server/index.js
@@ -6,9 +6,11 @@ let d = new db();
 
 let app = express();
 
+let port = process.env.PORT || 5000;
+
 app.use('/', express.static(__dirname + '/../public'));
 app.use('/', routes(d));
 
-app.listen(3000, () => {
-  console.log("listening on 3000");
+app.listen(5000, () => {
+  console.log("listening on 5000");
 })
